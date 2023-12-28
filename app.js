5fc31650bba08cb6cc79965d164286020343b18d
unknown <yadavraghbendra2057@gmail.com>
2023-12-28 22:47:10 +0545

    episode-01

---
+++ app.js
@@ -0,0 +1,20 @@
+//parent ko case maa \\
+const parent = React.createElement("div", {
+    id: "parent"
+},
+    React.createElement("div",
+        { id: "child" },[
+            React.createElement("h1", {}, " i am h1 tag") ,
+            React.createElement("h2", {}, " i am h2 tag"),
+            React.createElement("h2",{}, " ia ma an h2 tag"),
+        ]),
+        React.createElement("div",
+        { id: "child2" },[
+            React.createElement("h1", {}, " i am h1 tag") ,
+            React.createElement("h2", {}, " i am h2 tag"),
+            React.createElement("h2",{}, " ia ma an h2 tag"),
+        ]),
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
