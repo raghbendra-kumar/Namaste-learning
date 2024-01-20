efa903b900790d6f8a71f571c43d4d965d87c759
unknown <yadavraghbendra2057@gmail.com>
2024-01-20 15:16:48 +0545

    just doing bekar

---
+++ ap2p.js
@@ -0,0 +1,35 @@
+ //1st episode prgram of this by link of index.html
+ parent ko case maa \\
+const parent = React.createElement("div", {
+    id: "parent"
+},
+    React.createElement("div",
+        { id: "child" },[
+            React.createElement("h1", {}, " i am h1 tag") ,
+            React.createElement("h2", {}, " i am h2 tag"),
+            React.createElement("h2",{}, " ia ma an h2 tag"),
+        ]),
+        React.createElement("div",
+        { id: "child2" },[
+            React.createElement("h1", {}, " i am h1 tag") ,
+            React.createElement("h2", {}, " i am h2 tag"),
+            React.createElement("h2",{}, " ia ma an h2 tag"),
+        ]),
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+indexedDB.html 
+hello helll my dear world.
+<!-- <h1 id="heading" xyz="abc"> Hello world from learning world</h1> -->
+
+</div>
+<!-- <script> 
+ const heading =document.createElement("h2");
+ heading.innerHTML = "hello world from Javascript";
+ const root = document.getElementById("root");
+ root.appendChild(heading)
+
+</script> -->
--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-//parent ko case maa \\
-const parent = React.createElement("div", {
-    id: "parent"
-},
-    React.createElement("div",
-        { id: "child" },[
-            React.createElement("h1", {}, " i am h1 tag") ,
-            React.createElement("h2", {}, " i am h2 tag"),
-            React.createElement("h2",{}, " ia ma an h2 tag"),
-        ]),
-        React.createElement("div",
-        { id: "child2" },[
-            React.createElement("h1", {}, " i am h1 tag") ,
-            React.createElement("h2", {}, " i am h2 tag"),
-            React.createElement("h2",{}, " ia ma an h2 tag"),
-        ]),
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const Header = ( ) => { return (
+    <div className="header">
+        <div className="Logo-continer">
+            <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYOTKBcsiyByiaLLcwB7WOpsY1iU1lAXAgZg&usqp=CAU"/>
+            
+        </div>
+       <div className="nav-Items">
+        <ul>
+            <li> Home</li>
+            <li> About us</li>
+            <li> Contact Us</li>
+            <li> Cart</li>
+
+        </ul>
+       </div>
+    </div>
 );
-console.log(parent);
+};
+
+const AppLayout = ()=> {
+    return(
+    <div className ="app">
+        <header/>
+    </div>);
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
